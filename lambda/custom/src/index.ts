a810c6fd834a31d707b4d824d2aef1f8d840d9d2
alitaso345 <alitaso3456@gmail.com>
2019-01-03 19:16:58 +0900

    LaunchRequestHandlerとErrorHandlerを追加

--- lambda/custom/src/index.ts
+++ lambda/custom/src/index.ts
@@ -1,1 +1,35 @@
+import * as Alexa from 'ask-sdk-core'
+const skillBuilder = Alexa.SkillBuilders.custom()
 
+const LaunchRequestHandler: Alexa.RequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request
+    return request.type === 'LaunchRequest'
+  },
+  handle(handleInput) {
+    const welcomeMessage =
+      'クイズのジャンルを選択してください。声優、作曲家、ユニットのどれがよいですか？'
+    return handleInput.responseBuilder
+      .speak(welcomeMessage)
+      .reprompt(welcomeMessage)
+      .withSimpleCard('ミリオンライブクイズ', welcomeMessage)
+      .getResponse()
+  }
+}
+
+const ErrorHandler: Alexa.ErrorHandler = {
+  canHandle() {
+    return true
+  },
+  handle(handleInput, error) {
+    console.error(`ERROR: `, JSON.stringify(error, null, 2))
+    console.log(`handlerInput: `, JSON.stringify(handleInput, null, 2))
+
+    return handleInput.responseBuilder.getResponse()
+  }
+}
+
+export const handler = skillBuilder
+  .addRequestHandlers(LaunchRequestHandler)
+  .addErrorHandlers(ErrorHandler)
+  .lambda()
