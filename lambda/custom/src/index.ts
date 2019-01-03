bdbdb371fddd2e79ec321839eb584c891365a2c1
alitaso345 <alitaso3456@gmail.com>
2019-01-03 21:56:17 +0900

    QuizIntentを仮実装

--- lambda/custom/src/index.ts
+++ lambda/custom/src/index.ts
@@ -1,5 +1,10 @@
 import * as Alexa from 'ask-sdk-core'
 const skillBuilder = Alexa.SkillBuilders.custom()
+const startQuizMessage = 'ミリオンライブの声優に関するクイズを10問出題します。'
+const states = {
+  START: `_START`,
+  QUIZ: `_QUIZ`
+}
 
 const LaunchRequestHandler: Alexa.RequestHandler = {
   canHandle(handlerInput) {
@@ -8,11 +13,39 @@ const LaunchRequestHandler: Alexa.RequestHandler = {
   },
   handle(handleInput) {
     const welcomeMessage =
-      'クイズのジャンルを選択してください。声優、作曲家、ユニットのどれがよいですか？'
+      'アイドルマスターミリオンライブにクイズで遊べるスキルです、「クイズを開始」と言ってみてください。'
     return handleInput.responseBuilder
       .speak(welcomeMessage)
       .reprompt(welcomeMessage)
       .withSimpleCard('ミリオンライブクイズ', welcomeMessage)
+      .getResponse()
+  }
+}
+
+const QuizHandler: Alexa.RequestHandler = {
+  canHandle(handleInput) {
+    const request = handleInput.requestEnvelope.request
+    console.log('QuizHandler')
+    console.log(JSON.stringify(request))
+    return (
+      request.type === 'IntentRequest' &&
+      (request.intent.name === 'QuizIntent' ||
+        request.intent.name === 'AMAZON.StartOverIntent')
+    )
+  },
+  handle(handleInput) {
+    const attributes = handleInput.attributesManager.getSessionAttributes()
+    attributes.state = states.QUIZ
+    attributes.counter = 0
+    attributes.quizeScore = 0
+
+    const question = askQuestion(handleInput)
+    const speakOutput = startQuizMessage + question
+    const repromptOutput = question
+
+    return handleInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(repromptOutput)
       .getResponse()
   }
 }
@@ -30,6 +63,10 @@ const ErrorHandler: Alexa.ErrorHandler = {
 }
 
 export const handler = skillBuilder
-  .addRequestHandlers(LaunchRequestHandler)
+  .addRequestHandlers(LaunchRequestHandler, QuizHandler)
   .addErrorHandlers(ErrorHandler)
   .lambda()
+
+const askQuestion = handlerInput => {
+  return '第一問。春日未来の声優は誰でしょう？'
+}
