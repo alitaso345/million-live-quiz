b6182d920529e872bf56de48bf31e5b63e86100f
alitaso345 <alitaso3456@gmail.com>
2019-01-04 01:31:00 +0900

    Amazon.Actorは名字と名前の間のスペースなしで取得してくる

--- lambda/custom/src/index.ts
+++ lambda/custom/src/index.ts
@@ -30,45 +30,45 @@ interface ICharacter {
 }
 
 const data: ICharacter[] = [
-  { characterName: '春日 未来', characterKanaName: 'かすがみらい', voiceActorName: '山崎 はるか', voiceActorKanaName: 'やまざきはるか'},
-  { characterName: '木下 ひなた', characterKanaName: 'きのしたひなた', voiceActorName: '田村 奈央', voiceActorKanaName: 'たむらなお'},
+  { characterName: '春日 未来', characterKanaName: 'かすがみらい', voiceActorName: '山崎はるか', voiceActorKanaName: 'やまざきはるか'},
+  { characterName: '木下 ひなた', characterKanaName: 'きのしたひなた', voiceActorName: '田村奈央', voiceActorKanaName: 'たむらなお'},
   { characterName: 'ジュリア', characterKanaName: 'じゅりあ', voiceActorName: '愛美', voiceActorKanaName: 'あいみ' },
-  { characterName: '高山 紗代子', characterKanaName: 'たかやまさよこ', voiceActorName: '駒形 友梨', voiceActorKanaName: 'こまがたゆり' },
-  { characterName: '田中 琴葉', characterKanaName: 'たなかことは', voiceActorName: '種田 梨沙', voiceActorKanaName: 'たねだりさ' },
-  { characterName: '天空橋 朋花', characterKanaName: 'てんくうばしともか', voiceActorName: '小岩井 ことり', voiceActorKanaName: 'こいわいことり' },
-  { characterName: '箱崎 星梨花', characterKanaName: 'はこざきせりか', voiceActorName: '麻倉 もも', voiceActorKanaName: 'あさくらもも' },
-  { characterName: '松田 亜利沙', characterKanaName: 'まつだありさ', voiceActorName: '村川 梨衣', voiceActorKanaName: 'むらかわりえ' },
-  { characterName: '最上 静香', characterKanaName: 'もがみしずか', voiceActorName: '田所 あずさ', voiceActorKanaName: 'たどころあずさ' },
-  { characterName: '望月 杏奈', characterKanaName: 'もちづきあんな', voiceActorName: '夏川 椎菜', voiceActorKanaName: 'なつかわしいな' },
-  { characterName: '矢吹 可奈', characterKanaName: 'やぶきかな', voiceActorName: '木戸 衣吹', voiceActorKanaName: 'きどいぶき' },
-  { characterName: 'エミリー スチュアート', characterKanaName: 'えみりーすちゅあーと', voiceActorName: '郁原 ゆう', voiceActorKanaName: 'かはらゆう' },
-  { characterName: '大神 環', characterKanaName: 'おおがみたまき', voiceActorName: '稲川 英里', voiceActorKanaName: 'いながわえり' },
-  { characterName: '北上 麗花', characterKanaName: 'きたかみれいか', voiceActorName: '平山 笑美', voiceActorKanaName: 'ひらやまえみ' },
-  { characterName: '高坂 海美', characterKanaName: 'こうさかうみ', voiceActorName: '上田 麗奈', voiceActorKanaName: 'うえだれいな' },
-  { characterName: '佐竹 美奈子', characterKanaName: 'さたけみなこ', voiceActorName: '大関 英里', voiceActorKanaName: 'おおぜきえり' },
-  { characterName: '島原 エレナ', characterKanaName: 'しまばらえれな', voiceActorName: '角元 明日香', voiceActorKanaName: 'かくもとあすか' },
-  { characterName: '永吉 昴	', characterKanaName: 'ながよしすばる', voiceActorName: '斉藤 佑圭', voiceActorKanaName: 'さいとうゆか' },
-  { characterName: '野々原 茜', characterKanaName: 'ののはらあかね', voiceActorName: '小笠原 早紀', voiceActorKanaName: 'おがさわらさき' },
-  { characterName: '馬場 このみ', characterKanaName: 'ばばこのみ', voiceActorName: '高橋 未奈美', voiceActorKanaName: 'たかはしみなみ' },
-  { characterName: '福田 のり子', characterKanaName: 'ふくだのりこ', voiceActorName: '浜崎 奈々', voiceActorKanaName: 'はまさきなな' },
-  { characterName: '舞浜 歩', characterKanaName: 'まいはまあゆむ', voiceActorName: '戸田 めぐみ', voiceActorKanaName: 'とだめぐみ' },
-  { characterName: '真壁 瑞希', characterKanaName: 'まかべみずき', voiceActorName: '阿部 里果', voiceActorKanaName: 'あべりか' },
-  { characterName: '百瀬 莉緒', characterKanaName: 'ももせりお', voiceActorName: '山口 立花子', voiceActorKanaName: 'やまぐちりかこ' },
-  { characterName: '横山 奈緒', characterKanaName: 'よこやまなお', voiceActorName: '渡部 優衣', voiceActorKanaName: 'わたなべゆい' },
+  { characterName: '高山 紗代子', characterKanaName: 'たかやまさよこ', voiceActorName: '駒形友梨', voiceActorKanaName: 'こまがたゆり' },
+  { characterName: '田中 琴葉', characterKanaName: 'たなかことは', voiceActorName: '種田梨沙', voiceActorKanaName: 'たねだりさ' },
+  { characterName: '天空橋 朋花', characterKanaName: 'てんくうばしともか', voiceActorName: '小岩井ことり', voiceActorKanaName: 'こいわいことり' },
+  { characterName: '箱崎 星梨花', characterKanaName: 'はこざきせりか', voiceActorName: '麻倉もも', voiceActorKanaName: 'あさくらもも' },
+  { characterName: '松田 亜利沙', characterKanaName: 'まつだありさ', voiceActorName: '村川梨衣', voiceActorKanaName: 'むらかわりえ' },
+  { characterName: '最上 静香', characterKanaName: 'もがみしずか', voiceActorName: '田所あずさ', voiceActorKanaName: 'たどころあずさ' },
+  { characterName: '望月 杏奈', characterKanaName: 'もちづきあんな', voiceActorName: '夏川椎菜', voiceActorKanaName: 'なつかわしいな' },
+  { characterName: '矢吹 可奈', characterKanaName: 'やぶきかな', voiceActorName: '木戸衣吹', voiceActorKanaName: 'きどいぶき' },
+  { characterName: 'エミリー スチュアート', characterKanaName: 'えみりーすちゅあーと', voiceActorName: '郁原ゆう', voiceActorKanaName: 'かはらゆう' },
+  { characterName: '大神 環', characterKanaName: 'おおがみたまき', voiceActorName: '稲川英里', voiceActorKanaName: 'いながわえり' },
+  { characterName: '北上 麗花', characterKanaName: 'きたかみれいか', voiceActorName: '平山笑美', voiceActorKanaName: 'ひらやまえみ' },
+  { characterName: '高坂 海美', characterKanaName: 'こうさかうみ', voiceActorName: '上田麗奈', voiceActorKanaName: 'うえだれいな' },
+  { characterName: '佐竹 美奈子', characterKanaName: 'さたけみなこ', voiceActorName: '大関英里', voiceActorKanaName: 'おおぜきえり' },
+  { characterName: '島原 エレナ', characterKanaName: 'しまばらえれな', voiceActorName: '角元明日香', voiceActorKanaName: 'かくもとあすか' },
+  { characterName: '永吉 昴	', characterKanaName: 'ながよしすばる', voiceActorName: '斉藤佑圭', voiceActorKanaName: 'さいとうゆか' },
+  { characterName: '野々原 茜', characterKanaName: 'ののはらあかね', voiceActorName: '小笠原早紀', voiceActorKanaName: 'おがさわらさき' },
+  { characterName: '馬場 このみ', characterKanaName: 'ばばこのみ', voiceActorName: '高橋未奈美', voiceActorKanaName: 'たかはしみなみ' },
+  { characterName: '福田 のり子', characterKanaName: 'ふくだのりこ', voiceActorName: '浜崎奈々', voiceActorKanaName: 'はまさきなな' },
+  { characterName: '舞浜 歩', characterKanaName: 'まいはまあゆむ', voiceActorName: '戸田めぐみ', voiceActorKanaName: 'とだめぐみ' },
+  { characterName: '真壁 瑞希', characterKanaName: 'まかべみずき', voiceActorName: '阿部里果', voiceActorKanaName: 'あべりか' },
+  { characterName: '百瀬 莉緒', characterKanaName: 'ももせりお', voiceActorName: '山口立花子', voiceActorKanaName: 'やまぐちりかこ' },
+  { characterName: '横山 奈緒', characterKanaName: 'よこやまなお', voiceActorName: '渡部優衣', voiceActorKanaName: 'わたなべゆい' },
   { characterName: '伊吹 翼', characterKanaName: 'いぶきつばさ', voiceActorName: 'Machico', voiceActorKanaName: 'まちこ' },
-  { characterName: '北沢 志保', characterKanaName: 'きたざわしほ', voiceActorName: '雨宮 天', voiceActorKanaName: 'あまみやそら' },
-  { characterName: '篠宮 可憐', characterKanaName: 'しのみやかれん', voiceActorName: '近藤 唯', voiceActorKanaName: 'こんどうゆい' },
-  { characterName: '周防 桃子', characterKanaName: 'すおうももこ', voiceActorName: '渡部 恵子', voiceActorKanaName: 'わたなべけいこ' },
-  { characterName: '徳川 まつり', characterKanaName: 'とくがわまつり', voiceActorName: '諏訪 彩花', voiceActorKanaName: 'すわあやか' },
-  { characterName: '所 恵美', characterKanaName: 'ところめぐみ', voiceActorName: '藤井 ゆきよ', voiceActorKanaName: 'ふじいゆきよ' },
-  { characterName: '豊川 風花', characterKanaName: 'とよかわふうか', voiceActorName: '末柄 里恵', voiceActorKanaName: 'すえがらりえ' },
-  { characterName: '中谷 育', characterKanaName: 'なかたにいく', voiceActorName: '原嶋 あかり', voiceActorKanaName: 'はらしまあかり' },
-  { characterName: '七尾 百合子', characterKanaName: 'ななおゆりこ', voiceActorName: '伊藤 美来', voiceActorKanaName: 'いとうみく' },
-  { characterName: '二階堂 千鶴', characterKanaName: 'にかいどうちづる', voiceActorName: '野村 香菜子', voiceActorKanaName: 'のむらかなこ' },
-  { characterName: '宮尾 美也', characterKanaName: 'みやおみや', voiceActorName: '桐谷 蝶々', voiceActorKanaName: 'きりたにちょうちょ' },
-  { characterName: 'ロコ', characterKanaName: 'ろこ', voiceActorName: '中村 温姫', voiceActorKanaName: 'なかむらあつき' },
-  { characterName: '桜守 歌織', characterKanaName: 'さくらもりかおり', voiceActorName: '香里 有佐', voiceActorKanaName: 'こうりありさ' },
-  { characterName: '白石 紬', characterKanaName: 'しらいしつむぎ', voiceActorName: '南 早紀', voiceActorKanaName: 'みなみさき' },
+  { characterName: '北沢 志保', characterKanaName: 'きたざわしほ', voiceActorName: '雨宮天', voiceActorKanaName: 'あまみやそら' },
+  { characterName: '篠宮 可憐', characterKanaName: 'しのみやかれん', voiceActorName: '近藤唯', voiceActorKanaName: 'こんどうゆい' },
+  { characterName: '周防 桃子', characterKanaName: 'すおうももこ', voiceActorName: '渡部恵子', voiceActorKanaName: 'わたなべけいこ' },
+  { characterName: '徳川 まつり', characterKanaName: 'とくがわまつり', voiceActorName: '諏訪彩花', voiceActorKanaName: 'すわあやか' },
+  { characterName: '所 恵美', characterKanaName: 'ところめぐみ', voiceActorName: '藤井ゆきよ', voiceActorKanaName: 'ふじいゆきよ' },
+  { characterName: '豊川 風花', characterKanaName: 'とよかわふうか', voiceActorName: '末柄里恵', voiceActorKanaName: 'すえがらりえ' },
+  { characterName: '中谷 育', characterKanaName: 'なかたにいく', voiceActorName: '原嶋あかり', voiceActorKanaName: 'はらしまあかり' },
+  { characterName: '七尾 百合子', characterKanaName: 'ななおゆりこ', voiceActorName: '伊藤美来', voiceActorKanaName: 'いとうみく' },
+  { characterName: '二階堂 千鶴', characterKanaName: 'にかいどうちづる', voiceActorName: '野村香菜子', voiceActorKanaName: 'のむらかなこ' },
+  { characterName: '宮尾 美也', characterKanaName: 'みやおみや', voiceActorName: '桐谷蝶々', voiceActorKanaName: 'きりたにちょうちょ' },
+  { characterName: 'ロコ', characterKanaName: 'ろこ', voiceActorName: '中村温姫', voiceActorKanaName: 'なかむらあつき' },
+  { characterName: '桜守 歌織', characterKanaName: 'さくらもりかおり', voiceActorName: '香里有佐', voiceActorKanaName: 'こうりありさ' },
+  { characterName: '白石 紬', characterKanaName: 'しらいしつむぎ', voiceActorName: '南早紀', voiceActorKanaName: 'みなみさき' },
 ]
 
 const LaunchRequestHandler: Alexa.RequestHandler = {
@@ -158,6 +158,7 @@ const QuizAnswerHandler = {
       return response
         .speak(speakOutput)
         .reprompt(repromptOutput)
+        .withSimpleCard('答え', getDisplayalbeAnswer(item))
         .getResponse()
     } else {
       speakOutput += getFinalScore(attributes.quizeScore)
@@ -242,6 +243,9 @@ const getSpeechCon = (type: boolean) => {
 
 const getAnswer = (item: ICharacter) =>
   `<sub alias="${item.characterKanaName}">${item.characterName}</sub>の声優は<sub alias="${item.voiceActorKanaName}">${item.voiceActorName}</sub>さんでした！`
+
+const getDisplayalbeAnswer = (item: ICharacter) =>
+  `${item.characterName}の声優は${item.voiceActorName}さんでした！`
 
 const getCurrentScore = (score: number) => `現在の正解スコアは${score}です。`
 
